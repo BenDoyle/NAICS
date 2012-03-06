31a5ba2f7afef7bd6473d2290399c104537578a1
Ben Doyle <ben.doyle@jadedpixel.com>
2012-03-06 12:25:59 -0500

    file to generate structured csv

---
+++ process.rb
@@ -0,0 +1,27 @@
+require 'csv'
+require 'awesome_print'
+
+a = CSV.read(ARGV[0], col_sep: "\t")
+a.reject!{|e| e.first.nil? or e.first.strip.empty?}
+
+out = []
+a.each do |fields|
+
+  notes_regexp = /(CAN|MEX|US)/
+  name  = fields[1].strip.gsub(notes_regexp,'')
+  notes = fields[1].strip.match(notes_regexp) ? fields[1].strip.match(notes_regexp)[0] : ""
+  
+  out << {
+    level: fields[0].strip.size,
+    code:  fields[0].strip.to_i,
+    name:  name,
+    notes: notes,
+  }
+end
+
+CSV.open(ARGV[1], "wb") do |csv|
+  csv << %w[level code name notes]
+  out.each do |line|
+    csv << [line[:level],line[:code],line[:name],line[:notes]]
+  end
+end
